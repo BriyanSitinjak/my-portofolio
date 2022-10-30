5e60bc5f3ad5a4cfd73ecf91d1fc4e35608ef3eb
unknown <Imanbriyan@gmail.com>
2022-10-30 16:50:52 +0700

    Continue working on NavBar

---
+++ src/Component/NavbarComponent/NavbarComponent.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./NavbarComponent.scss";
+
+const NavbarComponent = () => {
+  return (
+    <div className="navComp">
+        <div className="nav-left">
+            <div className="nav-name">Briyan Sitinjak</div>
+            <span>Toggle</span>
+        </div>
+        <div className="nav-right">
+          <div className="nav-list">
+            <ul>
+              <li>Home</li>
+              <li>Services</li>
+              <li>Experience</li>
+              <li>Portofolio</li>
+              <li>Testimonial</li>
+            </ul>
+          </div>
+          <button className="button">
+            Contat Us
+          </button>
+        </div>
+    </div>
+  )
+}
+
+export default NavbarComponent
