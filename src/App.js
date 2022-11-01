b50fa17d85bb961cbd2ecc657ec049255fff231f
unknown <Imanbriyan@gmail.com>
2022-11-01 23:55:44 +0700

    Intro page for the left part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import NavbarComponent from './Component/NavbarComponent/NavbarComponent';
+import IntroComponent from './Component/IntroComponent/IntroComponent';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <IntroComponent />
     </div>
   );
 }
