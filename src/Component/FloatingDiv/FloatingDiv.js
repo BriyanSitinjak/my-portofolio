0778bc196a4b9b31e27bbaaff210004f008fd8a0
unknown <Imanbriyan@gmail.com>
2022-11-10 23:27:09 +0700

    Floating img

---
+++ src/Component/FloatingDiv/FloatingDiv.js
@@ -0,0 +1,16 @@
+import React from "react";  
+
+const FloatingDiv = ({ image }) => {
+  return (
+    <div className="floatingDiv">
+        <img src=""alt="" />
+        <span>
+            Web
+            <br />
+            Developer
+        </span>
+    </div>
+  )
+}
+
+export default FloatingDiv
